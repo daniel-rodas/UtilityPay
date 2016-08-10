476c0479398e3bd0342af96b037652e14fb31c3b
Daniel Rodas <daniel.rodas1@hotmail.com>
2016-08-10 02:14:19 -0700

    Add project files.

---
+++ UtilityPay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UtilityPay.MvcApplication" Language="C#" %>
---
+++ UtilityPay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UtilityPay.Startup))]
+namespace UtilityPay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

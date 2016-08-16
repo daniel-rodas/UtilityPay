2ee6bf1f9b064cec2c447f642e5b7064bef00c89
Daniel Rodas <daniel.rodas1@hotmail.com>
2016-08-16 16:59:05 -0700

    Added welcome screen.

---
+++ UtilityPay/Controllers/WelcomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UtilityPay.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        // GET: Welcome
+        public ActionResult Index()
+        {
+            ViewBag.ValueProposition = new[] { "Manage invoices.",
+                "Keep track of utility bills.",
+                "Split payments with roomates." };
+
+            ViewBag.NextButtonValue = new[] { "Continue", "Tell me more", "I'm interested" };
+
+            return View(ViewBag);
+        }
+
+
+        public ActionResult GetStarted()
+        {
+            ViewBag.ValueProposition = "Submit payments";
+
+            return View(ViewBag);
+        }
+    }
+}

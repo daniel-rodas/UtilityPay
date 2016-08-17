3f9c5cf5f49b9b731da98bc6c6cb50d4d8bb3982
Daniel Rodas <daniel.rodas1@hotmail.com>
2016-08-17 14:30:37 -0700

    Completed Welcome slide show.

--- UtilityPay/Controllers/WelcomeController.cs
+++ UtilityPay/Controllers/WelcomeController.cs
@@ -8,18 +8,44 @@ namespace UtilityPay.Controllers
 {
     public class WelcomeController : Controller
     {
-        // GET: Welcome
         public ActionResult Index()
         {
-            ViewBag.ValueProposition = new[] { "Manage invoices.",
-                "Keep track of utility bills.",
-                "Split payments with roomates." };
-
-            ViewBag.NextButtonValue = new[] { "Continue", "Tell me more", "I'm interested" };
+            ViewBag.ValueProposition = "Manage invoices.";
+            ViewBag.NextButtonValue = "Continue";
+            ViewBag.WelcomeStep = 2;
 
             return View(ViewBag);
         }
 
+        public ActionResult Step(int step)
+        {
+            var message = "";
+            var buttonText = "";
+            int NextWelcomeStep = 0;
+
+            if (step == 2)
+            {
+                message = "Keep track of utility bills.";
+                buttonText = "Tell me more";
+                NextWelcomeStep = 3;
+            }
+            else if (step == 3)
+            {
+                message = "Split payments with roomates.";
+                buttonText = "I'm interested";
+                NextWelcomeStep = 4;
+            }
+            else if (step == 4)
+            {
+                return RedirectToAction("GetStarted", "Welcome");
+            }
+
+            ViewBag.ValueProposition = message;
+            ViewBag.NextButtonValue = buttonText;
+            ViewBag.WelcomeStep = NextWelcomeStep;
+
+            return View("~/Views/Welcome/Index.cshtml", ViewBag);
+        }
 
         public ActionResult GetStarted()
         {
